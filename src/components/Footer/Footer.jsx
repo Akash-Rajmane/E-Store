7544da178646d539d314e3deade8e3fab76ddbc4
Akash-Rajmane <akashrajmane23@gmail.com>
2023-04-17 11:37:49 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div className="col">
+                    <div className="title">About</div>
+                    <div className="text">
+                    E Store is a global leader in lifestyle audio innovations, 
+                    professional audio solutions, and smart watches.
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Contact</div>
+                    <div className="c-item">
+                        <FaLocationArrow />
+                        <div className="text">
+                            M.G. Rd, Pune, 
+                            Maharashtra, 411057
+                        </div>
+                    </div>
+                    <div className="c-item">
+                        <FaMobileAlt />
+                        <div className="text">Phone: 0471 272 0261</div>
+                    </div>
+                    <div className="c-item">
+                        <FaEnvelope />
+                        <div className="text">Email: estore@gmail.com</div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="title">Categories</div>
+                    <span className="text">Headphones</span>
+                    <span className="text">Smart Watches</span>
+                    <span className="text">Bluetooth Speakers</span>
+                    <span className="text">Wireless Earbuds</span>
+                    
+                </div>
+                <div className="col">
+                    <div className="title">Pages</div>
+                    <span className="text">Home</span>
+                    <span className="text">About</span>
+                    <span className="text">Privacy Policy</span>
+                    <span className="text">Returns</span>
+                    <span className="text">Terms & Conditions</span>
+                    
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <div className="bottom-bar-content">
+                    <span className="text">
+                    © E Store 2023. PREMIUM E-COMMERCE
+                        SOLUTIONS.
+                    </span>
+                     <img src={Payment} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
+
---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALE</h1>
+                    <p>
+                    Take total control with 7.1 Channel Surround Sound for superior audio immersion.
+                     Get pinpoint positional accuracy of players and make your game come alive.
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
+
